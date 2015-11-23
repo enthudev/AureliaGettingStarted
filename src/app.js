4aee1712948dacb044d66a2aa218bfb949de80b2
enthudev <enthudev@live.in>
2015-11-23 08:58:06 +0530

    INitial commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+﻿export class app {
+    configureRouter(config, router) {
+        config.title = "Aurelia";
+        config.map([
+            { route: ['', 'welcome'], name: 'welcome', moduleId: 'welcome', nav: true, title: 'Welcome' }
+        ]);
+        this.router = router;
+    }
+}
